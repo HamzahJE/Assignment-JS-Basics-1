17a7d5b88090b8419a1c6f8d85c00f8914556173
Hamzah Jamal Eddin <hamzahjeddin@gmail.com>
2022-06-27 16:22:53 +0300

    validation

--- password.js
+++ password.js
@@ -3,22 +3,23 @@
 
 // Create a variable called password and set it’s value to a string of your choice.
 
-let password="hello12313243"
+let password="123kejrkjer3"
 
 // Write a conditional statement that checks if the password meets the following constraints:
 
 // At least 10 characters long
 
 // Contains at least one letter and one number
-
+let passwordArr= password.split("")
+console.log(passwordArr)
 // If the password meets the constraint, console log a success message.
 
 // If the password fails the constraint, console log a failure message.
-if(password.includes([0-9]) && password.includes([a-zA-z]) && password.length>=10){ 
+
+
+// Test out your conditional by changing the value of your password variable and running your file!
+if(password.match(/[a-zA-Z0-9][a-zA-Z][0-9]+/) && password.length>=10){ 
     console.log("SUCCESS, Password meets the minimum requirments ")
 }else{
     console.log("ERROR, Password does not meet the requirments ")
 }
-
-
-// Test out your conditional by changing the value of your password variable and running your file!
